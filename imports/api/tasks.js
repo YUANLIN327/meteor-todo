46f15bb421402e0eb96d82e28ca6758858f6f45c
Lin Yuan <yuanlin.dalian@gmail.com>
2018-09-27 11:49:46 -0400

    Remove unsecure. Define Meteor methods.

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,29 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+Meteor.methods({
+    'tasks.insert'(text) {
+        check(text, String);
+        if (!this.userId) {
+            throw new Meteor.Error('not-authrorized');
+        }
+        Tasks.insert({
+            text,
+            createdAt: Date.now(),
+            owner: this.userId,
+            username: Meteor.users.findOne(this.userId).userId
+        });
+    },
+    'tasks.remove'(taskId) {
+        check(taskId, String);
+        Tasks.remove(taskId);
+    },
+    'tasks.setChecked'(taskId, checked) {
+        check(taskId, String);
+        check(checked, Boolean);
+        Tasks.update(taskId, { $set: { checked } });
+    }
+})
 
 export const Tasks = new Mongo.Collection('tasks');
