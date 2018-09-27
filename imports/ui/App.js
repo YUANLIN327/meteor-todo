9afb0b01f04094303c3e07db50706199f8631488
Lin Yuan <yuanlin.dalian@gmail.com>
2018-09-27 10:14:57 -0400

    Storing template UI state.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -10,10 +10,13 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.inputRef = React.createRef();
+        this.state = {
+            hideCompleted: false,
+        }
     }
 
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        return this.props.tasks.filter(task => this.state.hideCompleted ? !task.checked : true).map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -25,11 +28,27 @@ class App extends Component {
         this.inputRef.current.value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted
+        });
+    }
+
     render() {
+        const incompleteCount = this.props.incompleteCount;
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List: {incompleteCount}</h1>
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
                     <form onSubmit={this.handleFormSubmit.bind(this)}>
                         <input type="text" ref={this.inputRef} placeholder="add some todo"></input>
                     </form>
@@ -46,5 +65,6 @@ class App extends Component {
 export default withTracker(() => {
     return {
         tasks: Tasks.find({}, { sort: { createAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     }
 })(App)
