523ee8cd697627e563902c7a7d0b0a89b1a6afbc
Lin Yuan <yuanlin.dalian@gmail.com>
2018-09-27 09:32:21 -0400

    add remove and update function

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,12 +1,31 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
+import {Tasks} from '../api/tasks';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
+    toggleCheck() {
+        Tasks.update(this.props.task._id, {
+            $set: { checked: !this.props.task.checked },
+        });
+    }
+
+    deleteTask() {
+        Tasks.remove(this.props.task._id);
+    }
+
     render() {
-        return ( 
-            <li> {this.props.task.text} </li>
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+        return (
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteTask.bind(this)}>delete</button>
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={this.toggleCheck.bind(this)}
+                />
+                <span className="text">{this.props.task.text}</span>
+            </li>
         );
     }
 }
